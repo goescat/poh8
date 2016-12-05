9510ac33f121a5478829ec24ec90e8fd1d69bf0c
goescat <goescat1024@gmail.com>
2016-12-05 16:50:01 +0800

    add poh1

---
+++ poh1.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int N;
+    string S;
+
+    cin >> N;
+    cin >> S;
+    for(int i=0; i<N; i++){
+    cout << S << endl;
+    }
+    return 0;
+}
